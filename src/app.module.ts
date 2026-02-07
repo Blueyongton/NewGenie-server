be9540789887574ed372b0d492a5f47113917244
chowon442 <chowon442@gmail.com>
2026-02-07 20:19:56 +0900

    chore: add modules and remove test files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { QuizModule } from './modules/quiz/quiz.module';
+import { UsersModule } from './modules/users/users.module';
+import { NewsModule } from './modules/news/news.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, QuizModule, UsersModule, NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
