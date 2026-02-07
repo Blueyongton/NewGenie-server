ebfe896500b0c95ab154268b20d06b6112364d3a
ehgus3193 <ehgus3193@naver.com>
2026-02-08 01:21:26 +0900

    fix : new 도메인 jwt 인증 추가

--- src/modules/news/news.controller.ts
+++ src/modules/news/news.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import {
     ApiTags,
     ApiOperation,
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/swagger';
 import { NewsService } from './news.service';
 import { CreateNewsDto } from './dtos/news.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @ApiTags('뉴스')
 @Controller('news')
@@ -14,6 +15,7 @@ export class NewsController {
     constructor(private readonly newsService: NewsService) {}
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     @ApiBearerAuth('access-token')
     @ApiOperation({ summary: '뉴스 생성' })
     @ApiResponse({ status: 201, description: '뉴스가 생성되었습니다.' })
