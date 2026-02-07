58e89b6f8d3ce6e5cb752872d852c2957b583c79
ehgus3193 <ehgus3193@naver.com>
2026-02-08 06:03:42 +0900

    feat: quiz 모듈 병합 에러 해결

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,28 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { QuizService } from './quiz.service';
 import { QuizController } from './quiz.controller';
+import { QuizSchedulerService } from './quiz-scheduler.service';
 import { Quiz } from './entities/quiz.entity';
 import { NewGenie } from './entities/newgenie.entity';
 import { GoalLog } from './entities/goal-log.entity';
 import { GoalArticle } from './entities/goal-article.entity';
 import { Goal } from './entities/goal.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Quiz, NewGenie, GoalLog, GoalArticle, Goal]),
+        ScheduleModule.forRoot(),
+        TypeOrmModule.forFeature([
+            Quiz,
+            NewGenie,
+            GoalLog,
+            GoalArticle,
+            Goal,
+            User,
+        ]),
     ],
     controllers: [QuizController],
-    providers: [QuizService],
-    exports: [QuizService],
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Quiz } from './entities/quiz.entity';
-import { GoalArticle } from '../news/entities/goal-article.entity';
-import { LlmService } from 'src/common/llm/llm.service';
-
-@Module({
-    imports: [TypeOrmModule.forFeature([Quiz, GoalArticle])],
-    controllers: [QuizController],
-    providers: [QuizService, LlmService],
+    providers: [QuizService, QuizSchedulerService],
+    exports: [QuizService, QuizSchedulerService],
 })
 export class QuizModule {}
